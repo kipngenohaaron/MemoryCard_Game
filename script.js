a0329688e568a9003c490be2c055a37e37ed7c07
kipngenohaaron <kipngenohaaron@gmail.com>
2023-08-10 01:42:20 +0300

    js added

---
+++ script.js
@@ -0,0 +1,36 @@
+const cards = document.querySelectorAll('.card');
+let flippedCards = [];
+let canFlip = true;
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    if (!canFlip || card.classList.contains('flipped')) return;
+    
+    card.classList.add('flipped');
+    card.querySelector('img').style.display = 'block';
+    flippedCards.push(card);
+
+    if (flippedCards.length === 2) {
+      canFlip = false;
+      setTimeout(() => checkMatch(), 1000);
+    }
+  });
+});
+
+function checkMatch() {
+  const [card1, card2] = flippedCards;
+  const card1Data = card1.getAttribute('data-card');
+  const card2Data = card2.getAttribute('data-card');
+
+  if (card1Data === card2Data) {
+    flippedCards = [];
+    canFlip = true;
+  } else {
+    card1.classList.remove('flipped');
+    card2.classList.remove('flipped');
+    card1.querySelector('img').style.display = 'none';
+    card2.querySelector('img').style.display = 'none';
+    flippedCards = [];
+    canFlip = true;
+  }
+}
